290cb41c7d2bde939f754a42b1e8b07252428ebc
ikorihn <16367098+ikorihn@users.noreply.github.com>
2022-02-02 00:00:13 +0900

    :zap: make cli with urfave/cli

--- cmd/xron/main.go
+++ cmd/xron/main.go
@@ -1,45 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"strings"
+	"log"
+	"os"
 
 	"github.com/ikorihn/xron"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	s := `<?xml version="1.0" encoding="UTF-8" ?>
-<books>
-    <book ID="extension1" available="yes">
-        <title>Book Title 1</title>
-        <price>100 - 200</price>
-        <note name="mynote">
-            <author id="20">Author 1</author>
-        </note>
-        <category name="novel" id="2" />
-        <empty />
-    </book>
-    <book ID="Book001" available="no">
-        <title>Book Title 2</title>
-        <price>400 - 500</price>
-    </book>
-    <book ID="extension2">
-        <title>Book Title 3</title>
-        <price>abc - 500</price>
-    </book>
-    <book available="soon">
-        <title>Book Title 5</title>
-        <price>a100 - c200</price>
-    </book>
-    <book ID="Book003" available="yes">
-        <title>Book Title 6</title>
-        <price>c100 - 200</price>
-    </book>
-</books>
-`
 
-	xpaths := xron.ConvertXmlToXpath(strings.NewReader(s))
-	for _, v := range xpaths {
-		fmt.Println(v)
+	var xmlFilePath string
+
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "file",
+				Usage:       "xml file to parse",
+				Aliases:     []string{"f"},
+				Destination: &xmlFilePath,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			if len(xmlFilePath) == 0 {
+				return errors.New("please specify file")
+			}
+			f, err := os.Open(xmlFilePath)
+			if err != nil {
+				return err
+			}
+			xpaths := xron.ConvertXmlToXpath(f)
+			for _, v := range xpaths {
+				fmt.Println(v)
+			}
+			return nil
+		},
 	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
